80abe115fe0b82aadc18747291782911c333b3f9
HXL3OZP_thdgit <HXL3OZP_thdgit@homedepot.com>
2022-10-02 14:43:20 -0700

    Oh its goin

--- pages/sections/LandingPage.js
+++ pages/sections/LandingPage.js
@@ -1,6 +1,6 @@
-import { AdaptiveDpr, AdaptiveEvents, BakeShadows, Float, MeshDistortMaterial, Plane, Scroll, Sphere } from "@react-three/drei";
+import { AdaptiveDpr, AdaptiveEvents, BakeShadows, Float, MeshDistortMaterial, Preload, RoundedBox, Scroll, Sphere } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { Bloom, DepthOfField, EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
+import { EffectComposer, Vignette } from "@react-three/postprocessing";
 import { animate, useMotionValue, useSpring } from "framer-motion";
 import { useRef } from "react";
 import * as THREE from "three";
@@ -8,83 +8,88 @@ import * as THREE from "three";
 
 export default function LandingPage () {
     
-    const sizeFactor = 1.25;
+    return (
+        <>
+            <pointLight position={[10, 10, 5]} intensity={0.5} />
+            
+            <ambientLight intensity={.1}/>
+
+            <SphereBlobs/>
+            <Preload/>
+
+            <EffectComposer>
+                <Vignette eskil={false} offset={0.1} darkness={.5} />
+            </EffectComposer>
+
+
+            <BakeShadows />
+            <AdaptiveDpr pixelated/>
+            <AdaptiveEvents />
+        </>
+    )
+}
+
+function SphereBlobs() {
+    const sizeFactor = 1;
 
     const sphereProperties = [
         {
             ref: useRef(),
             altRef: useRef(),
-            position: [0, 0, 0],
+            position: [2, -2, 2],
             altPosition: {
-                x: useSpring(0, { duration: 2500}),
-                y: useSpring(0, { duration: 2500}),
+                x: useSpring(2, { duration: 1000}),
+                y: useSpring(-2, { duration: 1000}),
             },
-            args: [1.75 * sizeFactor, 64, 64],
-            color: useMotionValue('#FF0000'),
-            altColor: useMotionValue('#00ff00')
+            args: [3 * sizeFactor, 32, 32],
+            color: useMotionValue('#a34100')
         },
         {
             ref: useRef(),
             altRef: useRef(),
-            position: [-2, 2, 0],
+            position: [-1, 4, 0],
             altPosition: {
-                x: useSpring(-2, { duration: 2500}),
-                y: useSpring(2, { duration: 2500}),
+                x: useSpring(-1, { duration: 1000}),
+                y: useSpring(4, { duration: 1000}),
             },
-            args: [1 * sizeFactor, 64, 64],
-            color: useMotionValue('#00FF00'),
-            altColor: useMotionValue('#0000ff')
+            args: [3 * sizeFactor, 32, 32],
+            color: useMotionValue('#F29F05')
         },
+
         {
             ref: useRef(),
             altRef: useRef(),
-            position: [4.5, 2, -2],
+            position: [-5, -1.5, -3],
             altPosition: {
-                x: useSpring(4.5, { duration: 2500}),
-                y: useSpring(2, { duration: 2500}),
+                x: useSpring(-5, { duration: 1000}),
+                y: useSpring(-1.5, { duration: 1000}),
             },
-            args: [1.5 * sizeFactor, 64, 64],
-            color: useMotionValue('#0000FF'),
-            altColor: useMotionValue('#FF0000')
-        },
-        {
-            ref: useRef(),
-            altRef: useRef(),
-            position: [-4, -1.5, -3],
-            altPosition: {
-                x: useSpring(-4, { duration: 2500}),
-                y: useSpring(-1.5, { duration: 2500}),
-            },
-            args: [2 * sizeFactor, 64, 64],
-            color: useMotionValue('#a34100'),
-            altColor: useMotionValue('#F2EBE3')
+            args: [2.5 * sizeFactor, 32, 32],
+            color: useMotionValue('#731919')
         },
     ]
 
+    const groupRef = useRef();
 
-    sphereProperties.forEach((sphere)=>{
-        animate(sphere.color, sphere.altColor.get(), { 
-            duration: 10,
-            repeat: Infinity,
-            repeatType: "reverse",
-        })
-    })
+    useFrame(({mouse, clock, delta})=>{
+        groupRef.current.rotation.x = clock.getElapsedTime() / 5;
+        groupRef.current.rotation.y = clock.getElapsedTime() / 5;
 
-    useFrame(({clock, mouse})=>{
+        groupRef.current.position.z = Math.sin(clock.getElapsedTime()) - 5;
+
         sphereProperties.forEach((sphere)=>{
-            sphere.ref.current.material.color = new THREE.Color( sphere.color.get())
+
             sphere.ref.current.rotation.y = THREE.MathUtils.lerp(
                 sphere.ref.current.rotation.y,
                 mouse.x * Math.PI,
-                0.005
+                0.003
             );
             sphere.ref.current.rotation.x = THREE.MathUtils.lerp(
                 sphere.ref.current.rotation.x,
                 mouse.y * Math.PI,
-                0.005
+                0.003
             );
             
-            // console.log(sphere.altPosition.x.get());
 
             sphere.altRef.current.position.x = sphere.altPosition.x.get();
             sphere.altRef.current.position.y = sphere.altPosition.y.get();
@@ -92,73 +97,52 @@ export default function LandingPage () {
     })
 
     return (
-        <>
-            <Scroll>
-                <pointLight position={[10, 10, 5]} intensity={0.5} />
+        <group ref={groupRef} position-z={-5}>
+            {/* Floating Spheres */}
+            {sphereProperties.map((sphere, index) => {
+                const randomFactor = Math.random() * 10 + index;
 
-                {/* Floating Spheres */}
-                {sphereProperties.map((sphere, index) => {
-                    const randomFactor = Math.random() * 10 + index;
-
-                    return (
-                        <Float key={index} floatIntensity={randomFactor} floatingRange={randomFactor}>
-                            <Sphere ref={sphere.ref} position={sphere.position} args={sphere.args}
-                                onPointerOver={(e) => { 
-                                    sphere.altPosition.x.set(sphere.position[0] + (.5 * Math.random() < 0.15 ? -1 : 1))
-                                    sphere.altPosition.y.set(sphere.position[1] + (.5 * Math.random() < 0.15 ? -1 : 1))
-                                 }} 
-                                onPointerOut={(e) => { 
-                                    sphere.altPosition.x.set(sphere.position[0])
-                                    sphere.altPosition.y.set(sphere.position[1])
+                return (
+                    <Float key={index} floatIntensity={randomFactor} floatingRange={randomFactor}>
+                        <Sphere ref={sphere.ref} position={sphere.position} args={sphere.args}
+                            onPointerOver={(e) => { 
+                                sphere.altPosition.x.set(sphere.position[0] + (.5 * Math.random() < 0.15 ? -1 : 1))
+                                sphere.altPosition.y.set(sphere.position[1] + (.5 * Math.random() < 0.15 ? -1 : 1))
                                 }} 
-                            >
-                                <MeshDistortMaterial
-                                    color={sphere.color.get()}
-                                    speed={1}
-                                    factor={1}
-                                    bumpScale={0.005}
-                                    clearcoat={1}
-                                    clearcoatRoughness={1}
-                                    radius={1}
-                                    distort={0.4}
-                                />
-                            </Sphere>
-                            <Sphere ref={sphere.altRef} position={sphere.position} args={sphere.args}>
-                                <MeshDistortMaterial
-                                    color={sphere.color.get()}
-                                    speed={1}
-                                    factor={1}
-                                    bumpScale={0.005}
-                                    clearcoat={1}
-                                    clearcoatRoughness={1}
-                                    radius={1}
-                                    distort={0.4}
-                                />
-                            </Sphere>
-                        </Float>
+                            onPointerOut={(e) => { 
+                                sphere.altPosition.x.set(sphere.position[0])
+                                sphere.altPosition.y.set(sphere.position[1])
+                            }} 
+                        >
+                            <MeshDistortMaterial
+                                toneMapped={false} fog={false}
+                                color={sphere.color.get()}
+                                speed={1}
+                                factor={1}
+                                bumpScale={0.005}
+                                clearcoat={1}
+                                clearcoatRoughness={1}
+                                radius={1}
+                                distort={0.5}
+                            />
+                        </Sphere>
+                        <Sphere ref={sphere.altRef} position={sphere.position} args={sphere.args}>
+                            <MeshDistortMaterial
+                                toneMapped={false} fog={false}
+                                color={sphere.color.get()}
+                                speed={1}
+                                factor={1}
+                                bumpScale={0.005}
+                                clearcoat={1}
+                                clearcoatRoughness={1}
+                                radius={1}
+                                distort={0.5}
+                            />
+                        </Sphere>
+                    </Float>
 
-                    )
-                })}
-
-                <EffectComposer>
-                    <DepthOfField focusDistance={1} focalLength={1} bokehScale={50} height={480}/>
-                    <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-                    <Noise opacity={0.1} />
-                    <Vignette eskil={false} offset={0.1} darkness={1} />
-                </EffectComposer>
-
-
-                <BakeShadows />
-                <AdaptiveDpr pixelated/>
-                <AdaptiveEvents />
-            </Scroll>
-            <Scroll html>
-                <div className="noise w-screen h-screen opacity-30 "/>
-                <div className="text-9xl font-bold fixed top-[25vh] left-[20vw] mix-blend-soft-light">
-                    <div className="text-center"> Mikaela Lakew </div>
-                    <div className="text-center"> Graphic Designer </div>
-                </div>
-            </Scroll>
-        </>
+                )
+            })}
+        </group>
     )
 }

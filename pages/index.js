d066336e2a01572706a62cf018bec6a3acc8001f
HXL3OZP_thdgit <HXL3OZP_thdgit@homedepot.com>
2022-10-03 16:27:51 -0700

    Oh its goin

--- pages/index.js
+++ pages/index.js
@@ -10,47 +10,19 @@ export default function Home() {
         <title>Mikaela Lakew | Graphic Designer</title>
         <meta name="description" content="Mikaela Personal Portfolio" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href='https://use.typekit.net/hcu8dkr.css'/>
       </Head>
 
-      <div class="bg-[#d8c1a7] snap-y snap-mandatory h-screen overflow-scroll">
-        <div className='snap-center'>
-          <Canvas 
-            performance={{ min: 0.1 }} gl={{ antialias: false }}
-            onCreated={({ gl }) => ((gl.shadowMap.autoUpdate = false), (gl.shadowMap.needsUpdate = true))}
-            style={{height: '120vh', width: '100vw', color: '#F2EBE3', filter: 'blur(50px)'}}>
-            <PresentationControls
-              global={false} // Spin globally or by dragging the model
-              cursor={true} // Whether to toggle cursor style on drag
-              snap={true} // Snap-back to center (can also be a spring config)
-              speed={1} // Speed factor
-              zoom={1} // Zoom factor when half the polar-max is reached
-              rotation={[0, 0, 0]} // Default rotation
-              polar={[0, Math.PI / 2]} // Vertical limits
-              azimuth={[-Infinity, Infinity]} // Horizontal limits
-              config={{ mass: 1, tension: 170, friction: 26 }} // Spring config
-            >
-              <LandingPage/>
-            </PresentationControls>
-            <Html fullscreen>
-              <div className="w-screen h-[120vh] opacity-20 bg-amber-800"/>
-            </Html>
-          </Canvas>
-          <div className="flex flex-col relative bottom-[85vh] justify-center snap-center text-9xl font-bold text-amber-100 mix-blend-soft-light">
-            <div className="text-center"> Mikaela Lakew </div>
-            <div className="text-center"> Graphic Designer </div>
-          </div>
-        </div>
+      <div className="bg-[#d8c1a7] snap-y snap-mandatory h-screen overflow-scroll">
+        <LandingPage/>
 
-
-
-
-        <div class="h-screen w-screen snap-center">
+        <div className="h-screen w-screen snap-start">
           <div>
             Yeet 
           </div>
         </div>
 
-        <div class="h-screen w-screen snap-center">
+        <div className="h-screen w-screen snap-center">
           <div>
             Yeet 
           </div>
--- pages/sections/LandingPage.js
+++ pages/sections/LandingPage.js
@@ -1,34 +1,77 @@
-import { AdaptiveDpr, AdaptiveEvents, BakeShadows, Float, MeshDistortMaterial, Preload, RoundedBox, Scroll, Sphere } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { AdaptiveDpr, AdaptiveEvents, BakeShadows, Float, Html, Instance, Instances, MeshDistortMaterial, Preload } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { EffectComposer, Vignette } from "@react-three/postprocessing";
-import { animate, useMotionValue, useSpring } from "framer-motion";
+import { motion, useSpring } from "framer-motion";
 import { useRef } from "react";
 import * as THREE from "three";
 
-
 export default function LandingPage () {
-    
     return (
-        <>            
-            <ambientLight intensity={.25}/>
+        <div className='snap-center max-h-[120vh]'>
+            <motion.div 
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1}}
+                viewport={{ once: true }}
+                style={{
+                    height: '110vh', 
+                    width: '100vw', 
+                    color: '#F2EBE3', 
+                    filter: 'blur(50px)'
+                  }}
+            >
+                <Canvas 
+                    performance={{ min: 0.1 }} gl={{ antialias: false }}
+                    onCreated={({ gl }) => ((gl.shadowMap.autoUpdate = false), (gl.shadowMap.needsUpdate = true))}>
 
-            <SphereBlobs/>
-            <Preload/>
+                    <pointLight intensity={1} position={[10, 10, 10]}/>
 
-            <EffectComposer>
-                <Vignette eskil={false} offset={0.1} darkness={.5} />
-            </EffectComposer>
+                    <SphereBlobs/>
+                    <Preload/>
 
+                    <EffectComposer>
+                        <Vignette eskil={false} offset={0.1} darkness={1} />
+                    </EffectComposer>
 
-            <BakeShadows />
-            <AdaptiveDpr pixelated/>
-            <AdaptiveEvents />
-        </>
+                    <BakeShadows />
+                    <AdaptiveDpr pixelated/>
+                    <AdaptiveEvents />
+                    <Html fullscreen>
+                        <div className="w-screen h-[120vh] opacity-20 bg-gradient-to-tr from-amber-700 to-[#F2EBE3]"/>
+                    </Html>
+                </Canvas>
+ 
+            </motion.div>
+            
+            <div className="noise relative" style={{bottom: '110vh'}}/>
+
+            <div className="relative bottom-[200vh] snap-center text-9xl font-bold text-black mix-blend-soft-light ">
+                <div className="h-36 overflow-hidden">
+                    <div className="flex justify-center"> 
+                        {`Mikaela Lakew`.split("").map((token, index)=>{
+                            return(
+                                <motion.div key={index} style={{margin: `0 ${10 * (token === ' ')}px`}} 
+                                    initial={{ opacity: 0, y: '100%' }} 
+                                    whileInView={{ opacity: 1, y:0, transition: {delay: index/20, duration: 1}}}>
+                                        {token}
+                                </motion.div>)
+                        })}
+                    </div>
+                  </div>
+                <motion.div className="flex justify-center"
+                    initial={{  x: '3%' }}
+                    whileInView={{x: 0, transition: {delay: 1.5, duration: 1}}}> 
+                        Graphic Design
+                        <motion.div
+                            initial={{ opacity: 0, y: '10%' }}
+                            whileInView={{ opacity: 1, y:0, transition: {delay: 1.75, duration: 1}}}>er</motion.div>
+                </motion.div>
+            </div>
+        </div>
     )
 }
 
 function SphereBlobs() {
-    const sizeFactor = 1.25;
+    let sizeFactor = 1.25;
 
     const sphereProperties = [
         {
@@ -36,22 +79,22 @@ function SphereBlobs() {
             altRef: useRef(),
             position: [2, -2, 2],
             altPosition: {
-                x: useSpring(2, { duration: 1000}),
-                y: useSpring(-2, { duration: 1000}),
+                x: useSpring(2, { duration: 750}),
+                y: useSpring(-2, { duration: 750}),
             },
-            args: [3 * sizeFactor, 32, 32],
-            color: useMotionValue('#a34100')
+            scale: 3.5,
+            color: useSpring('#a34100')
         },
         {
             ref: useRef(),
             altRef: useRef(),
             position: [-1, 4, 0],
             altPosition: {
-                x: useSpring(-1, { duration: 1000}),
-                y: useSpring(4, { duration: 1000}),
+                x: useSpring(-1, { duration: 750}),
+                y: useSpring(4, { duration: 750}),
             },
-            args: [3 * sizeFactor, 32, 32],
-            color: useMotionValue('#F29F05')
+            scale: 3,
+            color: useSpring('#F29F05')
         },
 
         {
@@ -59,35 +102,37 @@ function SphereBlobs() {
             altRef: useRef(),
             position: [-5, -1.5, -3],
             altPosition: {
-                x: useSpring(-5, { duration: 1000}),
-                y: useSpring(-1.5, { duration: 1000}),
+                x: useSpring(-5, { duration: 750}),
+                y: useSpring(-1.5, { duration: 750}),
             },
-            args: [2.5 * sizeFactor, 32, 32],
-            color: useMotionValue('#731919')
+            scale: 2.5,
+            color: useSpring('#731919')
         },
     ]
 
     const groupRef = useRef();
 
-    useFrame(({mouse, clock, delta})=>{
+    useFrame(({mouse, clock})=>{
         groupRef.current.rotation.x = clock.getElapsedTime() / 5;
         groupRef.current.rotation.y = clock.getElapsedTime() / 5;
 
-        groupRef.current.position.z = Math.sin(clock.getElapsedTime() / 2) - 5;
+        groupRef.current.position.z = Math.sin(clock.getElapsedTime() / 2) - 7;
 
         sphereProperties.forEach((sphere)=>{
 
             sphere.ref.current.rotation.y = THREE.MathUtils.lerp(
                 sphere.ref.current.rotation.y,
                 mouse.x * Math.PI,
-                0.003
+                0.007
             );
             sphere.ref.current.rotation.x = THREE.MathUtils.lerp(
                 sphere.ref.current.rotation.x,
                 mouse.y * Math.PI,
-                0.003
+                0.007
             );
             
+            sphere.ref.current.color.set( sphere.color.get() );
+            sphere.altRef.current.color.set( sphere.color.get() );
 
             sphere.altRef.current.position.x = sphere.altPosition.x.get();
             sphere.altRef.current.position.y = sphere.altPosition.y.get();
@@ -96,51 +141,47 @@ function SphereBlobs() {
 
     return (
         <group ref={groupRef} position-z={-5}>
-            {/* Floating Spheres */}
-            {sphereProperties.map((sphere, index) => {
-                const randomFactor = Math.random() * 10 + index;
+            <Instances>
+                <sphereGeometry args={[1.25, 16, 16]}/>
+                <MeshDistortMaterial toneMapped={false} fog={false}
+                    speed={1}
+                    factor={1}
+                    bumpScale={0.005}
+                    clearcoat={1}
+                    clearcoatRoughness={1}
+                    radius={1}
+                    distort={0.5}
+                />
+                {sphereProperties.map((sphere, index) => {
+                    const randomFactor = Math.random() * 20 + index;
 
-                return (
-                    <Float key={index} floatIntensity={randomFactor} floatingRange={randomFactor}>
-                        <Sphere ref={sphere.ref} position={sphere.position} args={sphere.args}
-                            onPointerOver={(e) => { 
-                                sphere.altPosition.x.set(sphere.position[0] + (.5 * Math.random() < 0.15 ? -1 : 1))
-                                sphere.altPosition.y.set(sphere.position[1] + (.5 * Math.random() < 0.15 ? -1 : 1))
+                    return (
+                        <Float key={index} floatIntensity={randomFactor} floatingRange={randomFactor}>
+                            <Instance
+                                ref={sphere.ref} 
+                                scale={sphere.scale}
+                                color={sphere.color.get()}
+                                position={sphere.position} 
+                                onPointerOver={(e) => { 
+                                    sphere.altPosition.x.set(sphere.position[0] + (Math.random() < 0.15 ? -2 : 2))
+                                    sphere.altPosition.y.set(sphere.position[1] + (Math.random() < 0.15 ? -2 : 2))
+                                    }} 
+                                onPointerOut={(e) => { 
+                                    sphere.altPosition.x.set(sphere.position[0])
+                                    sphere.altPosition.y.set(sphere.position[1])
                                 }} 
-                            onPointerOut={(e) => { 
-                                sphere.altPosition.x.set(sphere.position[0])
-                                sphere.altPosition.y.set(sphere.position[1])
-                            }} 
-                        >
-                            <MeshDistortMaterial
-                                toneMapped={false} fog={false}
-                                color={sphere.color.get()}
-                                speed={1}
-                                factor={1}
-                                bumpScale={0.005}
-                                clearcoat={1}
-                                clearcoatRoughness={1}
-                                radius={1}
-                                distort={0.5}
+                                onClick={(e) => {
+                                    sphere.color.set(`rgb(${randomBetween(90, 243)},${randomBetween(39, 180)},${randomBetween(6, 46)})`);
+                                }}
                             />
-                        </Sphere>
-                        <Sphere ref={sphere.altRef} position={sphere.position} args={sphere.args}>
-                            <MeshDistortMaterial
-                                toneMapped={false} fog={false}
-                                color={sphere.color.get()}
-                                speed={1}
-                                factor={1}
-                                bumpScale={0.005}
-                                clearcoat={1}
-                                clearcoatRoughness={1}
-                                radius={1}
-                                distort={0.5}
-                            />
-                        </Sphere>
-                    </Float>
+                            <Instance ref={sphere.altRef} color={sphere.color.get()} scale={sphere.scale} position={sphere.position} />
+                        </Float>
+                    )
+                })}
+            </Instances>
 
-                )
-            })}
         </group>
     )
 }
+
+const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
